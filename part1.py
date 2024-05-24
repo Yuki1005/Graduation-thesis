590a271dd5a6e037caca2fe1d1228026a905d0a2
Yuki1005 <yhiramatsu5@icloud.com>
2024-05-24 16:04:11 +0900

    s

--- part1.py
+++ part1.py
@@ -20,18 +20,24 @@ loc_list = cache_loc_list()
 
 #front_end
 
-location =  st.text_input('行きたいところ', placeholder='GoogleMap_URL', help='GoogleMapのURLを使用すること')
+image = st.image("trip_image.jpg")
 
+title = st.title("TriOpt")
 
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("追加",key=1):
-        split_url , zahyo = url_org(loc_list)
-        loc_list.append([urllib.parse.unquote(split_url[5]),float(zahyo[0].split("@")[1]),float(zahyo[1])])
-        location_time = pd.DataFrame(loc_list, columns=["地名","latitude","longitude"])
         
-        st.dataframe(location_time,height = 200)
-
-with col2:
-    if st.button("リセット",key=3):
-        loc_list.clear()
+with st.form("行きたいところ",clear_on_submit=True):
+    location =  st.text_input('行きたいところ', placeholder='GoogleMap_URL', help='GoogleMapのURLを使用すること')
+    col1, col2 = st.columns(2)
+    with col1:
+        submit_button = st.form_submit_button("追加")
+        if submit_button:
+            split_url , zahyo = url_org(loc_list)
+            loc_list.append([urllib.parse.unquote(split_url[5]),float(zahyo[0].split("@")[1]),float(zahyo[1])])
+            location_time = pd.DataFrame(loc_list, columns=["地名","latitude","longitude"])
+            
+            st.experimental_set_query_params()
+            st.dataframe(location_time,height = 200)
+    with col2:
+        reset_button = st.form_submit_button("リセット")
+        if reset_button:
+            loc_list.clear()
