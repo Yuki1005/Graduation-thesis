b91654265f3dfd767fac14405fcb63ec59829593
Yuki1005 <yhiramatsu5@icloud.com>
2024-05-27 22:40:22 +0900

    a

--- part1.py
+++ part1.py
@@ -4,8 +4,8 @@ import urllib.parse
 
 # 関数まとめ
 
-def url_org(loc_list):
-    url_data = location
+def url_org(url_google):
+    url_data = url_google
     split_url = url_data.split("/")
     zahyo = split_url[6].split(",")
     return split_url,zahyo
@@ -24,20 +24,23 @@ image = st.image("trip_image.jpg")
 
 title = st.title("TripOpt")
 
-        
+#　出発地点_目的地
+
+
+#　行きたいところ        
 with st.form("行きたいところ",clear_on_submit=True):
     location =  st.text_input('行きたいところ', placeholder='GoogleMap_URL', help='GoogleMapのURLを使用すること')
-    col1, col2 = st.columns(2)
-    with col1:
+    col1_2, col2_2 = st.columns(2)
+    with col1_2:
         submit_button = st.form_submit_button("追加")
         if submit_button:
-            split_url , zahyo = url_org(loc_list)
+            split_url , zahyo = url_org(location)
             loc_list.append([urllib.parse.unquote(split_url[5]),float(zahyo[0].split("@")[1]),float(zahyo[1])])
             location_time = pd.DataFrame(loc_list, columns=["地名","latitude","longitude"])
             
             st.experimental_set_query_params()
             st.dataframe(location_time,height = 200)
-    with col2:
+    with col2_2:
         reset_button = st.form_submit_button("リセット")
         if reset_button:
             loc_list.clear()
